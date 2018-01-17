496f03b6530f624104254c888dbccf16274285c4
Joe Ziemba <jziemba90@gmail.com>
2018-01-17 15:11:46 -0500

    contacts controller test

--- spec/controllers/contacts_controller_spec.rb
+++ spec/controllers/contacts_controller_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe ContactsController, type: :controller do
-
+  it 'should have an index method that renders the index template' do
+    get :index
+    assert_response :success
+    render_template :index
+  end
 end
