2225833adb1979840c882b801c8c4aaae804cb83
Joe Ziemba <jziemba90@gmail.com>
2018-01-17 15:42:13 -0500

    added ContactsContainer component

--- react/src/App.js
+++ react/src/App.js
@@ -2,10 +2,17 @@
 import React from 'react';
 import { Route, IndexRoute, Router, browserHistory } from 'react-router';
 
+// Component Imports
+import ContactsContainer from './containers/ContactsContainer'
+
 const App = (props) => {
 
   return(
-    <div>Oh Hey wassup</div>
+    <div>
+      <Router history={browserHistory}>
+        <Route path='/contacts' component={ContactsContainer} />
+      </Router>
+    </div>
   )
 }
 
---
+++ react/src/containers/ContactsContainer.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class ContactsContainer extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+
+    }
+  }
+
+  // Put your functions here
+  // functionName(argument) => {
+  //
+  // }
+
+  render() {
+
+    return(
+      <div>
+        YAY
+      </div>
+    )
+  }
+}
+
+export default ContactsContainer;
