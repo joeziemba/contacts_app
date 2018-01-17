83ca05e0c5d9a83ed83ebca58b64e3988adcf924
Joe Ziemba <jziemba90@gmail.com>
2018-01-17 12:20:02 -0500

    added React to Rails

---
+++ react/src/App.js
@@ -0,0 +1,14 @@
+// React Imports
+import React from 'react';
+import { Route, IndexRoute, Router, browserHistory } from 'react-router';
+
+import React from 'react';
+
+const App = (props) => {
+
+  return(
+    <div>Oh Hey wassup</div>
+  )
+}
+
+export default App;
