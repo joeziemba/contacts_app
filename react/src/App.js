70cd7abdb15e31ff9420784c53c793be76d01f6b
Joe Ziemba <jziemba90@gmail.com>
2018-01-19 15:50:29 -0500

    refactored rendering of contacts container in App component

--- react/src/App.js
+++ react/src/App.js
@@ -1,6 +1,5 @@
 // React Imports
 import React from 'react';
-import { Route, IndexRoute, Router, browserHistory } from 'react-router';
 
 // Component Imports
 import ContactsContainer from './containers/ContactsContainer'
@@ -9,9 +8,7 @@ const App = (props) => {
 
   return(
     <div>
-      <Router history={browserHistory}>
-        <Route path='/contacts' component={ContactsContainer} />
-      </Router>
+      <ContactsContainer />
     </div>
   )
 }
