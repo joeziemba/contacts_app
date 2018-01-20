c2dd37d82a4847c89f503b2091b56de4b1430fa9
Joe Ziemba <jziemba90@gmail.com>
2018-01-20 16:01:05 -0500

    styled components

--- react/src/components/ContactTile.js
+++ react/src/components/ContactTile.js
@@ -3,12 +3,22 @@ import React from 'react';
 const ContactTile = (props) => {
 
   return(
-    <div>
-      {props.firstName}
-      {props.lastName}
-      {props.email}
-      {props.phone}
-      {props.company}
+    <div className={`grid-x ${props.tileClass}`}>
+      <div className='cell medium-2'>
+        {props.firstName}
+      </div>
+      <div className='cell medium-2'>
+        {props.lastName}
+      </div>
+      <div className='cell medium-3'>
+        {props.email}
+      </div>
+      <div className='cell medium-2'>
+        {props.phone}
+      </div>
+      <div className='cell medium-3'>
+        {props.company}
+      </div>
     </div>
 
   )
