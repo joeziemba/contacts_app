bea0daff873844273853481993f75116e44cdba6
Joe Ziemba <jziemba90@gmail.com>
2018-01-21 17:51:48 -0500

    delete button tests

--- react/test/components/ContactTileSpec.js
+++ react/test/components/ContactTileSpec.js
@@ -3,15 +3,18 @@ import ContactTile from '../../src/components/ContactTile';
 describe('ContactTile', () => {
 //// SETUP /////////////////
   let wrapper;
+  let deleteSpy = jasmine.createSpy('deleteContact')
   beforeEach( () => {
     wrapper = mount(
       <ContactTile
+        id='1'
         firstName='First Name'
         lastName='Last Name'
         email='Email'
         phone='Phone'
         company='Company'
         tileClass='contact-tile'
+        deleteContact={deleteSpy}
       />
     )
   })
@@ -28,5 +31,15 @@ describe('ContactTile', () => {
   it('should have the grid-x and the tileClass prop as className', () => {
     expect(wrapper.find('.grid-x')).toBePresent()
     expect(wrapper.find('.grid-x')).toHaveClassName('contact-tile')
+  })
+
+  it('should display a delete button with an id of props.id', () => {
+    expect(wrapper.find('.delete-button')).toBePresent()
+    expect(wrapper.find('.delete-button').props().id).toEqual('1')
+  })
+
+  it('should trigger the delete function', () => {
+    wrapper.find('.delete-button').simulate('click')
+    expect(deleteSpy).toHaveBeenCalled()
   })
 })
